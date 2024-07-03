3068ed7ee96cc43579da10552465ca4138a6df88
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-02 23:28:28 -0400

    When loop

--- src/keyWord/DataTypes.kt
+++ src/keyWord/DataTypes.kt
@@ -1,18 +1,20 @@
 package keyWord
 
-class DataTypes {
+fun main(){
+//class DataTypes {
+
         val myNum = 5             // Int
         val myDoubleNum = 5.99    // Double
         val myLetter = 'D'        // Char
         val myBoolean = true      // Boolean
         val myText = "Hello"      // String
 
-    fun main(){
+
         println(myNum)
         println(myDoubleNum)
         println(myLetter)
         println(myBoolean)
         println(myText)
-    }
+  //  }
 
 }
