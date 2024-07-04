ea286f86e1df9dc3f188d744866c0cbb4b93c48a
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-03 22:33:10 -0400

    if else

---
+++ src/keyWord/ifElse.kt
@@ -0,0 +1,11 @@
+package keyWord
+
+fun main(args: Array<String>){
+    val number = -10
+
+    if (number > 0) {
+        print("Positive number")
+    } else {
+        print("Negative number")
+    }
+}
